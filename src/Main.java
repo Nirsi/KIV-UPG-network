a8eee1c63fdc600240d1363db6f5e2d36989c649
Enrich Silen <enrich.silen@gmail.com>
2018-03-12 11:50:11 +0100

    Module correction

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,22 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main
 {
     public static void main(String[] args) {
         WaterNetwork waterNetwork = new WaterNetwork();
 
+        JFrame frame = new JFrame();
         DrawComponents dc = new DrawComponents(waterNetwork);
-        dc.paintComponent();
+        dc.setPreferredSize(new Dimension(800,800));
+
+        frame.add(dc);
+
+        frame.setTitle("UPG - Semestrální vykreslení");
+        frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
     }
 }
