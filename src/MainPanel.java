2f01a401b67e6187b75b16c256e4967f436687fe
Enrich Silen <enrich.silen@gmail.com>
2018-04-10 00:57:21 +0200

    Cleanup in MainPanel

--- src/MainPanel.java
+++ src/MainPanel.java
@@ -26,13 +26,7 @@ public class MainPanel extends JPanel {
         Translator.getInstance().setTranslatedWidth(getWidth() - reservoirWidth);
         Translator.getInstance().setTranslatedHeight(getHeight() - reservoirWidth);
 
-
-        DrawReservoir dr = new DrawReservoir(g2);
-        DrawPipes dp = new DrawPipes(g2);
-        DrawNodes dn = new DrawNodes(g2);
-        DrawValves dv = new DrawValves(g2);
-        DrawArrow da = new DrawArrow(g2);
-        DrawNetworkComponents dnc = new DrawNetworkComponents(g2);
+        DrawNetworkComponents drawNetworkComponents = new DrawNetworkComponents(g2);
 
         double maxX = 0, maxY = 0;
         for (NetworkNode nn : wn.getAllNetworkNodes()) {
@@ -52,25 +46,21 @@ public class MainPanel extends JPanel {
 
 
         for (Pipe p : wn.getAllPipes()) {
-            dnc.drawPipes(p,reservoirWidth,reservoirHeight);
-            //dp.draw(p, reservoirWidth, reservoirHeight);
-            dnc.drawValves(p, reservoirWidth, reservoirHeight);
-            //da.draw(p, reservoirWidth,reservoirHeight);
-            dnc.drawArrows(p, reservoirWidth, reservoirHeight);
+            drawNetworkComponents.drawPipes(p,reservoirWidth,reservoirHeight);
+            drawNetworkComponents.drawValves(p, reservoirWidth, reservoirHeight);
+            drawNetworkComponents.drawArrows(p, reservoirWidth, reservoirHeight);
         }
 
         for (NetworkNode Nn : wn.getAllNetworkNodes()) {
             if (Nn instanceof Reservoir) {
-                //dr.draw((Reservoir) Nn, reservoirWidth, reservoirHeight);
-                dnc.drawReservoirs((Reservoir) Nn, reservoirWidth, reservoirHeight);
+                drawNetworkComponents.drawReservoirs((Reservoir) Nn, reservoirWidth, reservoirHeight);
 
             }
         }
 
         for (NetworkNode nn : wn.getAllNetworkNodes()) {
             if (!(nn instanceof Reservoir)) {
-                //dn.draw(nn, 50, 50);
-                dnc.drawNodes(nn,50,50);
+                drawNetworkComponents.drawNodes(nn, 50, 50);
             }
         }
 
